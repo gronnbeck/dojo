c52b8bfd15b66a8f613266dff64a06a9255a9fd2
Ken Grønnbeck <kengroenn@gmail.com>
2012-03-14 23:06:22 -0700

    able to sort files with odd number of lines

--- parasort/psort.py
+++ parasort/psort.py
@@ -42,37 +42,54 @@ def next2sort():
 
 def sort():
 	next = next2sort()
-	if not next:
-		return
-	subprocess.call(["sort -f " + next + " > " + next + ".sorted"], shell=True)
-	sortedlock.acquire()
-	remove(next)
-	Sorted.append(next + ".sorted")
-	sortedlock.release()
+	while next:
+		subprocess.call(["sort -f " + next + " > " + next + ".sorted"], shell=True)
+		sortedlock.acquire()
+		remove(next)
+		Sorted.append(next + ".sorted")
+		sortedlock.release()
+		next = next2sort()
 
 def pair2merge():
 	sortedlock.acquire()
 	pair = False
-	if len(Sorted) == 2:
+	if len(Sorted) >= 2:
 		pair = (Sorted.pop(), Sorted.pop())
 	sortedlock.release()
 	return pair
 
-def merge():
+def merge(final=False):
 	pair = pair2merge()
 	if not pair:
 		return
-	subprocess.call(["sort -fm " + pair[0] + " " + pair[1] + " > " + file + ".sorted"], shell=True)
+	if final:
+		output = file + ".sorted"
+	else:
+		output = pair[0] + ".merged"
+	subprocess.call(["sort -fm " + pair[0] + " " + pair[1] + " > " + output], shell=True)
 	remove(pair[0])
 	remove(pair[1])
+	sortedlock.acquire()
+	if not final:
+		Sorted.append(output)
+	sortedlock.release()
 
 def run():
 	sort()
 	merge()
 	sortedlock.acquire()
-	if len(Sorted) == 0:
-		event.set()
+	len_sorted = len(Sorted)
 	sortedlock.release()
+	
+	fileslock.acquire()
+	len_files = len(Files)
+	fileslock.release()
+	print "Here we go"
+	print len(Sorted)
+	print len(Files)
+	if len_sorted == 2 and len_files == 0:
+		merge(True)
+		event.set()
 
 split(file)
 
